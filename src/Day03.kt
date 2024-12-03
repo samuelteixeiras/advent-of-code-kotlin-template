3c6fa4c055c6d8b5b35a91d48c8e51dd649bbe15
samuel teixeira <samuelteixeiras@gmail.com>
2024-12-03 22:00:06 +0000

    day 03 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,46 +1,39 @@
-
 fun main() {
-    var lines = readInput("files/day03")
+    val lines = readInput("day03")
 
     // find all matches for mul(digits,digits)
-    val regex = """mul\((?<first>\d+),(?<second>\d+)\)""".toRegex()
+    val regex = """mul\((?<first>\d{1,3}),(?<second>\d{1,3})\)""".toRegex()
     var total = 0
-//    lines.map{ line:String ->
-//        var matchResult = regex.find(line)
-//
-//        if (matchResult != null) {
-//            while(matchResult?.groups?.get("first") != null) {
-//                val first = matchResult.groups["first"]?.value?.toInt()
-//                val second = matchResult.groups["second"]?.value?.toInt()
-//                total += first!! * second!!
-//
-//                matchResult = matchResult.next()
-//            }
-//        }
-//    }
-//    println(total)
-
-    // part 2
-
-    val regex2 = """don't.*?do\(\)""".toRegex()
-    val regex3 = """don't.*$""".toRegex()
-
-    total = 0;
     lines.map{ line:String ->
-        println(line)
-        var replaced = line.replace(regex2, "")
-        replaced = replaced.replace(regex3, "")
-        println(replaced)
-        println("------")
-        var matchResult = regex.find(replaced)
+        var matchResult = regex.find(line)
 
         if (matchResult != null) {
             while(matchResult?.groups?.get("first") != null) {
-                val first = matchResult!!.groups["first"]?.value?.toInt()
-                val second = matchResult!!.groups["second"]?.value?.toInt()
+                val first = matchResult.groups["first"]?.value?.toInt()
+                val second = matchResult.groups["second"]?.value?.toInt()
                 total += first!! * second!!
 
-                matchResult = matchResult!!.next()
+                matchResult = matchResult.next()
+            }
+        }
+    }
+    println(total)
+
+    // part 2
+    total = 0
+    var enabled = true
+    val mul = """mul\((?<first>\d{1,3}),(?<second>\d{1,3})\)"""
+    val doReg = """do\(\)"""
+    val dontReg = """don't\(\)"""
+    """$mul|$doReg|$dontReg""".toRegex().findAll(lines.joinToString()).forEach { match ->
+        when (match.value) {
+            "don't()" -> enabled = false
+            "do()" -> enabled = true
+            else -> if (enabled) {
+                val first = match.groups["first"]!!.value.toInt()
+                val second = match.groups["second"]!!.value.toInt()
+
+                total += first * second
             }
         }
     }
